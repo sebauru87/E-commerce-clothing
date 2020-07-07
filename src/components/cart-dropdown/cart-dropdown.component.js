ccb43f19793bc8a133dfb51cd72aef4402fa1958
Sebastian <Ferreira>
2020-07-07 17:22:02 -0300

    add npm reselect. Add cart selector to memorize cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custon-button.component';
 
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.styles.scss';
 
@@ -21,8 +22,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
